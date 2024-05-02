ad8dc40f1e9d03d74390668a60194591f69adcee
shohan-pherones <shohan.sub.56@gmail.com>
2024-05-02 20:59:08 +0600

    search page api req

--- src/app/search/[city]/page.tsx
+++ src/app/search/[city]/page.tsx
@@ -1,5 +1,22 @@
+"use client";
+
+import { useRestaurantPublic } from "@/hooks/useRestaurantPublic";
+
 const SearchPage = ({ params }: { params: { city: string } }) => {
-  return <div>User searched for {params.city}</div>;
+  const { results } = useRestaurantPublic(params.city);
+
+  return (
+    <div>
+      User searched for {params.city}{" "}
+      <span>
+        {results?.data.map((restaurant) => (
+          <span key={restaurant._id}>
+            found- {restaurant.restaurantName}, {restaurant.city}
+          </span>
+        ))}
+      </span>
+    </div>
+  );
 };
 
 export default SearchPage;
