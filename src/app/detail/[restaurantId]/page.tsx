64642ca81a071ab2e52eb532e1d6914fc609ed39
shohan-pherones <shohan.sub.56@gmail.com>
2024-05-16 20:57:29 +0600

    add to cart

--- src/app/detail/[restaurantId]/page.tsx
+++ src/app/detail/[restaurantId]/page.tsx
@@ -1,18 +1,59 @@
 "use client";
 
 import MenuItem from "@/components/MenuItem";
+import OrderSummery from "@/components/OrderSummery";
 import RestaurantInfo from "@/components/RestaurantInfo";
 import Error from "@/components/shared/Error";
 import Loading from "@/components/shared/Loading";
 import { AspectRatio } from "@/components/ui/aspect-ratio";
+import { Card } from "@/components/ui/card";
 import { useGetPublicRestaurant } from "@/hooks/useGetPublicRestaurant";
+import { MenuItem as TMenuItem } from "@/types";
 import Image from "next/image";
+import { useState } from "react";
+
+export interface CartItem {
+  _id: string;
+  name: string;
+  price: number;
+  quantity: number;
+}
 
 const RestaurantDetailPage = ({
   params,
 }: {
   params: { restaurantId: string };
 }) => {
+  const [cartItems, setCartItems] = useState<CartItem[]>([]);
+
+  const addToCart = (menuItem: TMenuItem) => {
+    setCartItems((prev) => {
+      const existingCartItem = prev.find((item) => item._id === menuItem._id);
+
+      let updatedCartItems;
+
+      if (existingCartItem) {
+        updatedCartItems = prev.map((item) =>
+          item._id === menuItem._id
+            ? { ...item, quantity: item.quantity + 1 }
+            : item
+        );
+      } else {
+        updatedCartItems = [
+          ...prev,
+          {
+            _id: menuItem._id,
+            name: menuItem.name,
+            price: menuItem.price,
+            quantity: 1,
+          },
+        ];
+      }
+
+      return updatedCartItems;
+    });
+  };
+
   const { restaurant, isLoading } = useGetPublicRestaurant(params.restaurantId);
 
   if (isLoading) {
@@ -38,8 +79,18 @@ const RestaurantDetailPage = ({
           <RestaurantInfo restaurant={restaurant} />
           <span className="text-2xl font-bold tracking-tight">Menu</span>
           {restaurant.menuItems.map((item, index) => (
-            <MenuItem key={index + item.name} item={item} />
+            <MenuItem
+              key={index + item.name}
+              item={item}
+              addToCart={() => addToCart(item)}
+            />
           ))}
+        </div>
+
+        <div>
+          <Card>
+            <OrderSummery restaurant={restaurant} cartItems={cartItems} />
+          </Card>
         </div>
       </div>
     </div>
---
+++ src/components/OrderSummery.tsx
@@ -0,0 +1,57 @@
+import { CartItem } from "@/app/detail/[restaurantId]/page";
+import { Restaurant } from "@/types";
+import { CardContent, CardHeader, CardTitle } from "./ui/card";
+import { Badge } from "./ui/badge";
+import { Separator } from "./ui/separator";
+
+interface Props {
+  restaurant: Restaurant;
+  cartItems: CartItem[];
+}
+
+const OrderSummery = ({ cartItems, restaurant }: Props) => {
+  const getTotalCost = () => {
+    const totalInCent = cartItems.reduce(
+      (total, item) => (total += item.price * item.quantity),
+      0
+    );
+
+    const totalWithDelivery = totalInCent + restaurant.deliveryPrice;
+
+    return (totalWithDelivery / 100).toFixed(2);
+  };
+
+  return (
+    <>
+      <CardHeader>
+        <CardTitle className="text-2xl font-bold flex justify-between tracking-tight">
+          <span>Your Order</span>
+          <span>${getTotalCost()}</span>
+        </CardTitle>
+      </CardHeader>
+      <CardContent className="flex flex-col gap-5">
+        {cartItems.map((item) => (
+          <div key={item._id} className="flex justify-between">
+            <span>
+              <Badge variant="outline" className="mr-2">
+                {item.quantity}
+              </Badge>
+              {item.name}
+            </span>
+            <span className="flex items-center gap-2">
+              ${((item.price * item.quantity) / 100).toFixed(2)}
+            </span>
+          </div>
+        ))}
+        <Separator />
+        <div className="flex justify-between">
+          <span>Delivery</span>
+          <span>${(restaurant.deliveryPrice / 100).toFixed(2)}</span>
+        </div>
+        <Separator />
+      </CardContent>
+    </>
+  );
+};
+
+export default OrderSummery;
